85843e5b5327709a34eb00d8fb52aa4d29d4e820
Sophia Fang <sophiaqfang@gmail.com>
2018-07-01 03:32:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function getRepositories(){
   const req = new XMLHttpRequest()
+  req.addEventListener("load", showRespositories);
   req.open("GET", 'http://api.github.com/users/octocat/repos')
   req.send()
 }
